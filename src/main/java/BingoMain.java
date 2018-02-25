5383e6ab63dea7e516083fb41de3279e2181d540
Suzan Shalan <suzan.shalan@gmail.com>
2018-02-25 20:34:30 +0100

    Added players counters generation

--- src/main/java/BingoMain.java
+++ src/main/java/BingoMain.java
@@ -1,19 +1,67 @@
+import com.beust.jcommander.internal.Lists;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-public class BingoMain implements Runnable{
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
-    public static void main(String [] args){
-       new BingoMain().run();
+@Slf4j
+public class BingoMain implements Runnable {
+
+    public static void main(String[] args) {
+        new BingoMain().run();
     }
 
     @Override
     public void run() {
+        // create a scanner so we can read the command-line input
+        Scanner scanner = new Scanner(System.in);
+
         log.debug("Welcome to Bingo game!");
-        FileMaker fileMaker = new FileMaker("counters","Bingo_counters.txt");
+        System.out.println("What is your name?");
+        String name = scanner.next();
+        System.out.println(String.format("Hi %s, welcome to Bingo game!", name));
+        final int numberOfPlayers = howManyWillPlay(scanner);
+        final List <String> players = whoWillPlay(scanner, numberOfPlayers);
+
+        try {
+            generatePullingNumbers();
+            generatePlayersNumbers(players);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.debug("We are done. Bye bye!");
+        }
+
+    private void generatePlayersNumbers(List<String> players) throws IOException {
+        int NumberOfContersPerPlayer = 100 / players.size();
+        FileMaker fileMaker = new FileMaker("counters", "Bingo_players_counters.txt");
+        fileMaker.createDirectory();
+        FileWriter fileWriter = new FileWriter(fileMaker.createFileUnderDirectory());
+        List<Integer> allocatedNumbers= Lists.newArrayList();
+        fileWriter.generateCounters(players, NumberOfContersPerPlayer, allocatedNumbers);
+    }
+
+    private void generatePullingNumbers() {
+        FileMaker fileMaker = new FileMaker("counters", "Bingo_counters.txt");
         fileMaker.createDirectory();
         FileWriter fileWriter = new FileWriter(fileMaker.createFileUnderDirectory());
         fileWriter.generateCounters();
-        log.debug("We are done. Bye bye!");
+    }
+
+    private int howManyWillPlay(Scanner scanner) {
+        System.out.println("Give the number of the players. " +
+                "Please separate your answer with a single space");
+        return Integer.valueOf(scanner.next());
+    }
+
+    private List<String> whoWillPlay(Scanner scanner, int numberOfPlayers) {
+        System.out.println("Give the name of the players. " +
+                "Please separate your answer with a single space");
+        List <String> players = Lists.newArrayList();
+        for(int i = 0; i<numberOfPlayers; i++){
+            players.add(scanner.next());
+        }
+        return players;
     }
 }
--- src/main/java/FileWriter.java
+++ src/main/java/FileWriter.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Random;
 
 @Slf4j
 @AllArgsConstructor
@@ -29,6 +31,45 @@ public class FileWriter {
                 log.warn("Unable to access file {}", file.getName());
             }
 
+        }
+    }
+
+    public List<Integer> generateCounters(List <String> players, int numberOfContersPerPlayer, List<Integer> allocatedCounters) throws IOException {
+        if (file.isFile() && file.exists()) {
+            file.setWritable(true);
+            try {
+                BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(file.getName()));
+                for(String player : players) {
+                    writer.write(player);
+                    writer.newLine();
+                    for (int i = 1; i <= numberOfContersPerPlayer; i++) {
+                        Random random = new Random();
+                        addARandomNumber(random, allocatedCounters, writer, i);
+
+                    }
+                    writer.newLine();
+                    writer.newLine();
+                }
+                log.debug("Added counters to file {}.", file.getName());
+                writer.close();
+            }catch (IOException e) {
+                log.warn("Unable to access file {}", file.getName());
+            }
+
+        }
+        return allocatedCounters;
+    }
+
+    private void addARandomNumber(Random random, List<Integer> allocatedCounters, BufferedWriter writer, int iterator) throws IOException {
+        int randomNumber = random.nextInt(100) + 1;
+        if (!allocatedCounters.contains(randomNumber)) {
+            allocatedCounters.add(randomNumber);
+            writer.write(randomNumber <= 10 ? randomNumber + EXTRA_TABB : randomNumber + TABB);
+            if (iterator % 10 == 0) {
+                writer.newLine();
+            }
+        } else {
+            addARandomNumber(random, allocatedCounters, writer, iterator);
         }
     }
 }
